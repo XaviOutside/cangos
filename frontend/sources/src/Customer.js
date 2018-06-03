168ab6ea834851ba7b7a63b759fd95316b40f15a
Xavi B <xavierbrgr@gmail.com>
2018-06-03 21:46:24 +0200

    Adding CORS in order to send a POST from frontend.

--- frontend/sources/src/Customer.js
+++ frontend/sources/src/Customer.js
@@ -2,43 +2,50 @@ import React, { Component } from 'react';
 import './Customer.css';
 
 const API = 'http://localhost:8081';
-//const API = 'http://customer.api';
 
 class Customer extends Component {
   constructor() {
     super();
+
+    this.state = {
+      username: '',
+      email: '',
+      birthdate: ''
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    //const data = new FormData(event.target);
-    
+  
     fetch(API + '/api/values/', {
       method: 'POST',
       headers: {
-        Accept: 'application/json',
         'Content-Type': 'application/json',
+        'Accept': 'application/json'
       },
-      body: JSON.stringify({
-        username: 'yourValue',
-        email: 'yourOtherValue',
-      }),
-    });
+      body: JSON.stringify(this.state)
+    }).then((res) => res.json())
+    .then((data) =>  console.log(this.state))
+    .catch((err)=>console.log(err));
+  }
 
+  handleChange (evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor="username">Enter username</label>
-        <input id="username" name="username" type="text" />
+        <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleChange} />
 
         <label htmlFor="email">Enter your email</label>
-        <input id="email" name="email" type="email" />
+        <input id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
 
         <label htmlFor="birthdate">Enter your birth date</label>
-        <input id="birthdate" name="birthdate" type="text" />
+        <input id="birthdate" name="birthdate" type="text" value={this.state.birthdate} onChange={this.handleChange} />
 
         <button>Send data!</button>
       </form>
